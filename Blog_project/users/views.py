e975e847d1366de39c6bfce42bb28fdb533d39ab
Mahmoud <mmshehatt@gmail.com>
2021-03-30 21:02:33 +0200

    Adding Blog_Project Folder

---
+++ Blog_project/users/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render , redirect
+from django.contrib.auth .forms import UserCreationForm
+
+# Create your views here.
+
+# ******************* register function *******
+def register(request):
+    form = UserCreationForm()
+    context={
+        'RegisterForm':form
+    }
+    return render(request , 'users/register.html' , context)
+
+# ******************* login function *******
+def login(request):
+    form = UserCreationForm()
+    context={
+        'RegisterForm':form
+    }
+    return render(request , 'users/register.html' , context)
